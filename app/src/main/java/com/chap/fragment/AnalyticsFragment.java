10683bd49a3bc13b120cd67548fbb2276eb81af7
dnjunge <dnjunge001@gmail.com>
2017-06-15 09:14:32 -0400

    Country Overview completed with addition of country level chororpleth maps

--- app/src/main/java/com/chap/fragment/AnalyticsFragment.java
+++ app/src/main/java/com/chap/fragment/AnalyticsFragment.java
@@ -1,7 +1,6 @@
 package com.chap.fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,83 +8,83 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.chap.R;
-import com.chap.activity.HealthIndicatorViewActivity;
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 
+
 public class AnalyticsFragment extends Fragment {
-    private Spinner mapSelectSpinner;
-    WebView mapVIEW;
+    private MapView mapView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(getActivity(), "pk.eyJ1IjoiZG5qdW5nZTAwMSIsImEiOiJjajMycjE4MnQwMDA4MzJwOWFlZHZkcnZqIn0.8oZi7nVN072ikpOqM5HbEQ");
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_analytics, container, false);
     }
 
-
-
     public void onViewCreated (View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mapVIEW = (WebView)view.findViewById(R.id.map_webview);
-        mapVIEW.getSettings().setJavaScriptEnabled(true);
-        mapVIEW.loadUrl("http://kenyachap.info/index.html");
-
-        mapSelectSpinner = (Spinner) view.findViewById(R.id.map_indicator_spinner);
-        mapSelectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // selectedIndicator = (String) (indicatorsList.getItemAtPosition(position));
-             String  selectedIndicator = parent.getItemAtPosition(position).toString();
-                Toast.makeText(getActivity(), selectedIndicator, Toast.LENGTH_SHORT).show();
-
-            }
-
-            @SuppressWarnings("unused")
-            public void onClick(View v) {
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-
-            ;
-        });
-
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
-
--- app/src/main/java/com/chap/fragment/CountryOverviewFragment.java
+++ app/src/main/java/com/chap/fragment/CountryOverviewFragment.java
@@ -1,86 +1,118 @@
 package com.chap.fragment;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+        import android.widget.AdapterView;
+        import android.widget.Spinner;
+        import android.widget.Toast;
 
-import com.chap.R;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-
+        import com.chap.R;
+        import com.chap.activity.HealthIndicatorViewActivity;
+        import com.mapbox.mapboxsdk.maps.MapView;
 
 public class CountryOverviewFragment extends Fragment {
-    private MapView mapView;
+    private Spinner mapSelectSpinner;
+    WebView mapVIEW;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(getActivity(), "pk.eyJ1IjoiZG5qdW5nZTAwMSIsImEiOiJjajMycjE4MnQwMDA4MzJwOWFlZHZkcnZqIn0.8oZi7nVN072ikpOqM5HbEQ");
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_country_overview, container, false);
     }
 
+
+
     public void onViewCreated (View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mapView = (MapView) view.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+        mapVIEW = (WebView)view.findViewById(R.id.map_webview);
+        mapVIEW.getSettings().setJavaScriptEnabled(true);
+       // mapVIEW.loadUrl("http://www.kenyachap.info/population.html");
+
+        mapSelectSpinner = (Spinner) view.findViewById(R.id.map_indicator_spinner);
+        mapSelectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String  selectedIndicator = parent.getItemAtPosition(position).toString();
+                switch (selectedIndicator){
+                    case "Population":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/population.html");
+                        break;
+                    case "Health Spending Per Person":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/health_spending.html");
+                        break;
+                    case "Human Development Index":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/human_development_index.html");
+                        break;
+                    case "Physicians Density":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/physician_density.html");
+                        break;
+                    case "Tuberculosis Prevalence":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/tuberculosis_prevalence.html");
+                        break;
+                    case "Percent Poverty Rate":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/percent_poverty_rate.html");
+                        break;
+                    case "Access To Electricity Rate":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/access_to_electricity_rate.html");
+                        break;
+                    case "Gender Inequality Index":
+                        mapVIEW.loadUrl("http://www.kenyachap.info/gender_inequality_index.html");
+                        break;
+                    default:
+                        mapVIEW.loadUrl("http://www.kenyachap.info/population.html");
+                }
+
+                Toast.makeText(getActivity(), selectedIndicator, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @SuppressWarnings("unused")
+            public void onClick(View v) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            ;
+        });
+
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
+
+
